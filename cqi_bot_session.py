6df17eb503740cdb4d3e1c7365471adcfefc66f0
James LeDoux <jledoux@jamess-macbook.lan>
2018-01-21 17:06:37 -0800

    update scraper to step through all coffee pages

--- cqi_bot_session.py
+++ cqi_bot_session.py
@@ -1,13 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
 import pandas as pd
 import time
 
-login_email = 'your_email'
-login_password = 'your_password'
+login_email = 'your_email_here'
+login_password = 'your_password_here'
 
 # open chromedriver
-driver = webdriver.Chrome('/Users/jledoux/Downloads/chromedriver')
+driver = webdriver.Chrome('your_chromedriver_location_here')
 time.sleep(2)
 
 # navigate to login page
@@ -28,49 +29,71 @@ time.sleep(2)
 
 # navigate to coffees page, then to arabicas page containing links to all quality reports 
 coffees = driver.find_element_by_xpath('//html/body/header/nav[@id="main"]/div[@class="container"]/div[@class="in"]/a[@href="/coffees"]').click()
-time.sleep(2)
+time.sleep(3)
 driver.find_element_by_link_text('Arabica Coffees').click()
+time.sleep(3)
 
-# process a coffee report - pull all data and export to csv 
+# these values can be changed if this breaks midway through collecting data to pick up close to where you left off
+page = 0
+coffeenum = 0
 
-#table = driver.find_element_by_xpath('//table')
-for i in driver.find_elements_by_xpath("//tr"):
-	print(i.get_attribute('row'))
+while True:
+	print('page {}'.format(page))
 
-test_page = driver.find_elements_by_xpath("//tr")[4].click() # click on 4th coffee as an example 
+	# 50 rows in these tables * 7 columns per row = 350 cells. Every 7th cell clicks through to that coffee's data page
+	for i in range(0,350,7):
+		time.sleep(4)
 
-tables = driver.find_elements(By.TAG_NAME, "table")
+		# paginate back to the desired page number
+		# don't think there's a way around this - the back() option goes too far back
+		# some page numbers aren't available in the ui, but 'next' always is unless you've reached the end 
+		for p_num in range(page):
+			page_buttons = driver.find_elements_by_class_name('paginate_button')
+			page_buttons[-1].click() # the 'next' button
+			time.sleep(1)
+			page_buttons = driver.find_elements_by_class_name('paginate_button')
 
-# loop over all coffee reports on the page, processing each one and writing to csv
+		# select the cell to click through to the next coffee-data page
+		time.sleep(2) # this next line errors out sometimes, maybe it needs more of a time buffer 
+		test_page=driver.find_elements_by_xpath('//td')[i].click()
+		time.sleep(2)
+		print('rows: ')
+		print(len(driver.find_elements_by_xpath("//tr")))
+		tables = driver.find_elements(By.TAG_NAME, "table")
 
-i = 1 # make this increment once im iterating through all coffees
-j = 0
-for i in tables:
-	#t = BeautifulSoup(tables[i].get_attribute('innerHTML'), "html.parser")
-	t = BeautifulSoup(i.get_attribute('outerHTML'), "html.parser")
-	print(t)
-	df = pd.read_html(str(t))
-	name = 'coffee_{}_table_{}.csv'.format(i,j)
-	df[0].to_csv(name)
-	j += 1
+		# loop over all coffee reports on the page, processing each one and writing to csv
+		print('tables: ')
+		print(len(tables))
+		j = 0
+		for tab in tables:
+			try:
+				t = BeautifulSoup(tab.get_attribute('outerHTML'), "html.parser")
+				#print(t)
+				df = pd.read_html(str(t))
+				name = 'coffee_{}_table_{}.csv'.format(coffeenum,j)
+				df[0].to_csv(name)
+				print(name)
+			except:
+				# only one's needed but I want this to be onoxious since it's the only way I'm logging this currently
+				print('ERROR: {} failed'.format(name))
+				print('ERROR: {} failed'.format(name))
+				print('ERROR: {} failed'.format(name))
+				print('ERROR: {} failed'.format(name))
+			j += 1
 
+		# go back to page with all other coffee results
+		#driver.back() # note: this isn't working as expected, manually going back to pg 1 via url instead
+		driver.get('https://database.coffeeinstitute.org/coffees/arabica')
+		time.sleep(2)
+		coffeenum += 1
 
-# skip to next page when done 
-
-
+	page += 1
+	if page == 27:
+		break
 
 
 # close the driver
 driver.close()
-
-
-
-
-
-
-
-
-
 
 
 
